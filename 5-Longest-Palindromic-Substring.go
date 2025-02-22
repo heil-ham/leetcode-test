66498bebbb571845c4ef3ccec9769a7f1fb92943
Ilham Fitri Ardiansyah <91858899+heil-ham@users.noreply.github.com>
2025-02-22 17:11:51 +0700

    [5] [Time Beats: 5.02%] [Memory Beats: 5.02%] - LeetPush

---
+++ 5-Longest-Palindromic-Substring.go
@@ -0,0 +1,33 @@
+func longestPalindrome(s string) string {
+    longestPalin := string(s[0])
+    
+    
+    for i := 0; i<len(s); i++ {
+        
+        if len(s) > 2 && (len(longestPalin) > len(s)/2) || (len(longestPalin) > len(s)-i) {
+            return longestPalin
+        }
+        substring := string(s[i])
+        for j := i+1; j<len(s); j++ {
+            substring += string(s[j])
+            if isPalindrome(substring) && (len(substring) > len(longestPalin)) {
+                longestPalin = substring
+            }
+        }
+    }
+
+    return longestPalin
+}
+
+func isPalindrome(s string) bool {
+    invertedIndex := len(s)-1
+    
+    for i:=0; i<int(len(s)/2); i++ {
+        if s[i] != s[invertedIndex] {
+            return false
+        }
+        invertedIndex--
+    }
+    
+    return true
+}
